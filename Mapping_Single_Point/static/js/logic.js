74c4d69d7dbda3dc45d6d78cf4b348d39f9a6514
Alejandra Villarreal <alejandra.y.villarreal21@gmail.com>
2022-08-07 22:27:28 -0500

    practice with markers and circleMarker

--- Mapping_Single_Point/static/js/logic.js
+++ Mapping_Single_Point/static/js/logic.js
@@ -9,11 +9,27 @@ let map = L.map("mapid", {
     zoom: 4
   });
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
+{
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
-});
+}).addTo(map);
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// add a layer with a marker to LA
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// change marker to a circle
+// L.circle([34.0522, -118.2437],{
+//     radius: 1000
+// }).addTo(map);
+
+
+//change marker to circleMarker
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
