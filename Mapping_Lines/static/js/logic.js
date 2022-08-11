870384420bfda0c5e30f76a92eaada11c578c156
Alejandra Villarreal <alejandra.y.villarreal21@gmail.com>
2022-08-11 15:55:47 -0500

    mapping lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,14 +2,26 @@
 console.log("working");
 
 // We create the map object with options.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+   
+
+
+  //Coordinates for each point to be used in the line
+  let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
-//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
 {
@@ -52,8 +64,8 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // ];
 
 // get data from cities.js
-var cityData = cities;
-console.log(cities);
+// var cityData = cities;
+// console.log(cities);
 
 // Loop through the cities array and create one marker for each city.
 // cityData.forEach(function(city) {
@@ -64,13 +76,13 @@ console.log(cities);
 // });
 
 // change the marker to multiple circles for each data point
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/200000,
-    color: "orange",
-    fillColor: '#F39C12'
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: city.population/200000,
+//     color: "orange",
+//     fillColor: '#F39C12'
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+// .addTo(map);
+// });
